9f27ec33415c0b40d11b2db938e9df5219187459
FactionCube <snugglebat@gmail.com>
2020-07-04 21:33:22 +1000

    Minor typo fix.

--- Cryptopals/Exercise_1/Set1_challenge2.ml
+++ Cryptopals/Exercise_1/Set1_challenge2.ml
@@ -13,7 +13,7 @@ let computeXor sa sb =
   let open Core in
   let sal = hxch2i (String.to_list sa) and sbl = hxch2i (String.to_list sb) in 
   let xor = List.map2_exn ~f:(fun a b -> a lxor b) sal sbl in
-  i2hxch xor |>String.of_char_list
+  i2hxch xor |> String.of_char_list
   in computeXor a b
 
 (* val ans : string = "746865206b696420646f6e277420706c6179"  *)
