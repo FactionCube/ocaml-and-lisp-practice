28e9564d3b3803c00d9d94525615a845dfc3bd68
FactionCube <neytiri@Synoptic.local>
2020-08-14 20:58:27 +1000

    Code clean up - for evaluation in utop.

--- Cryptopals/Exercise_1/Set1_challenge2.ml
+++ Cryptopals/Exercise_1/Set1_challenge2.ml
@@ -1,3 +1,4 @@
+
 (*
 #require "core";;
 #require "iter";;
@@ -8,13 +9,13 @@ open HexCraft
 let a = "1c0111001f010100061a024b53535009181c" and b = "686974207468652062756c6c277320657965"
 ;;
 
-
 let computeXor sa sb =
   let open Core in
-  let sal = hxch2i (String.to_list sa) and sbl = hxch2i (String.to_list sb) in 
+  let sal = hxch2i (String.to_list sa) and sbl = hxch2i (String.to_list sb) in
   let xor = List.map2_exn ~f:(fun a b -> a lxor b) sal sbl in
   i2hxch xor |> String.of_char_list
   in computeXor a b
+;;
 
 (* val ans : string = "746865206b696420646f6e277420706c6179"  *)
 
@@ -24,9 +25,9 @@ open Cryptokit
 let hex s = transform_string (Hexa.decode()) s
 let hexbytes s = Bytes.of_string (hex s)
 let tohex s = transform_string (Hexa.encode()) s
+;;
 
 let hex_a = hexbytes a and hex_b = hexbytes b in
-xor_bytes hex_a 0 hex_b 0 18; 
-
-let ans = tohex (Bytes.to_string hex_b) in ans
+xor_bytes hex_a 0 hex_b 0 18;
+tohex (Bytes.to_string hex_b)
 ;;
