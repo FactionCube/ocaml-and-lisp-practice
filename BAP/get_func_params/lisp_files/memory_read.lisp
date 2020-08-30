bfa9b7fb5a182e4d49ee37735e8dbdb51487019e
FactionCube <snugglebat@gmail.com>
2020-08-30 21:50:48 +1000

    Experimenting wth pointer read.

--- BAP/get_func_params/lisp_files/memory_read.lisp
+++ BAP/get_func_params/lisp_files/memory_read.lisp
@@ -1,13 +1,36 @@
+
+;;(defmacro read-word (t a)
+;;  "(read-word T A) reads a word of type T at address A"
+;;  (let ((p a)
+;;        (x (memory-read p))
+;;        (n (-1 (sizeof t))))
+;;    (while n
+;;      (incr p)
+;;      (decr n)
+;;      (set x (endian concat x (memory-read p))))
+;;    x))
+
+
 (defparameter *struct-address* 0x201020)
 
-;; My knowledge of lisp is rudimentary, so I've yet to work out
-;; how to step through ptr below to read its bytes.
+;; This guy isn't working yet, at least I amnot seeing the
+;; printed msg in 'incident';.
 (defmethod loading (ptr)
   (when (= ptr *struct-address*)
-    (memory-read ptr )
-    (msg ">>>> struct read $0" ptr)
+    (let ((p ptr)
+	  (x (memory-read ptr))
+	  (n (-1 (sizeof int))))
+      (while n
+	     (incr p)
+	     (decr n)
+	     (set x (endian concat x (memory-read p))))
+      (msg "***** $0 $1" p x))
+;;    (memory-read ptr )
+;;    (msg ">>>> struct read $0" ptr)
+;;    (read-word (int ptr))
     )
 )
+
 
 ;;(defun memory-written (x a)
 ;;	(declare (advice :before memory-write))
