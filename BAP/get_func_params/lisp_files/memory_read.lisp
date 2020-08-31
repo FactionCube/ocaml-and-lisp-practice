069ce5a9d01c85106b573a85087bf583eecb5d83
FactionCube <snugglebat@gmail.com>
2020-08-31 19:39:56 +1000

    Minor changes.

--- BAP/get_func_params/lisp_files/memory_read.lisp
+++ BAP/get_func_params/lisp_files/memory_read.lisp
@@ -1,4 +1,3 @@
-
 ;;(defmacro read-word (t a)
 ;;  "(read-word T A) reads a word of type T at address A"
 ;;  (let ((p a)
@@ -15,22 +14,11 @@
 
 ;; This guy isn't working yet, at least I amnot seeing the
 ;; printed msg in 'incident';.
-(defmethod loading (ptr)
-  (when (= ptr *struct-address*)
-    (let ((p ptr)
-	  (x (memory-read ptr))
-	  (n (-1 (sizeof int))))
-      (while n
-	     (incr p)
-	     (decr n)
-	     (set x (endian concat x (memory-read p))))
-      (msg "***** $0 $1" p x))
-;;    (memory-read ptr )
-;;    (msg ">>>> struct read $0" ptr)
-;;    (read-word (int ptr))
+(defmethod call-return (name ptr ret)
+  (when 
+    (and (= ptr *struct-address*) (not (points-to-null ret)))
+      (msg "***** $0 $1 " name ret))
     )
-)
-
 
 ;;(defun memory-written (x a)
 ;;	(declare (advice :before memory-write))
--- BAP/get_func_params/test.c
+++ BAP/get_func_params/test.c
@@ -8,7 +8,6 @@ typedef unsigned char byte;
 struct foo_t {
 	unsigned int x;
 	unsigned int y;
-	unsigned int z;
 };
 
 int func0 ( byte a, byte b, byte c )
@@ -25,7 +24,6 @@ struct foo_t* set_struct_a ( struct foo_t * foo)
 {
 	foo->x = 0xABABABAB;
 	foo->y = 0xCDCDCDCD;
-	foo->z = 0xDEDEDEDE;
 
 	return foo;
 }
@@ -34,19 +32,24 @@ struct foo_t* set_struct_b ( struct foo_t * foo)
 {
 	foo->x = 0xDEADBEEF;
 	foo->y = 0xCAFEBABE;
-	foo->z = 0xD00DBAAF;
 	return foo;
 }
 
 
 int main () {
 
-	struct foo_t * foo = (struct foo_t *) malloc(sizeof (struct foo_t));
+	struct foo_t * boo = (struct foo_t *) malloc(sizeof (struct foo_t));
+	struct foo_t * coo = (struct foo_t *) malloc(sizeof (struct foo_t));
 	char ret = func0 ( 0x99 , 0x88 , 0x77);
-	struct foo_t * boo = set_struct_a ( foo);
-	struct foo_t * coo = set_struct_b ( foo);
+	set_struct_a ( boo);
+	set_struct_b ( coo);
 
-	free (foo);
+	printf("boo.x = %x  coo.x = %x\n", boo->x, coo->x);
+
+	free (boo);
+	free (coo);
+
 	return 0;
+
 }
 
