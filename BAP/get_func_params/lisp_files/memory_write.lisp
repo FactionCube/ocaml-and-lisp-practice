46a0ec4ee558cef55baee111fbbfacff4c7ce774
FactionCube <snugglebat@gmail.com>
2020-09-05 17:42:55 +1000

    Experimenting with writing bytes to memory.

---
+++ BAP/get_func_params/lisp_files/memory_write.lisp
@@ -0,0 +1,28 @@
+(defparameter *my-special-address0* 0x201010)
+(defparameter *my-special-address1* 0x201011)
+(defparameter *my-special-address2* 0x201012)
+(defparameter *my-special-address3* 0x201013)
+
+(defmethod loading (ptr)
+    (when (= ptr *my-special-address0*)
+          (memory-write ptr 0xFA)
+    )
+)
+
+(defmethod loading (ptr)
+    (when (= ptr *my-special-address1*)
+          (memory-write ptr 0xFB)
+    )
+)
+
+(defmethod loading (ptr)
+    (when (= ptr *my-special-address2*)
+          (memory-write ptr 0xFC)
+    )
+)
+
+(defmethod loading (ptr)
+    (when (= ptr *my-special-address3*)
+          (memory-write ptr 0xFD)
+    )
+)
